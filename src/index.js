e5a912f0e4a9d081516cc5353dc6f29f6ccd8a97
Anton Usatiuk <160845263+UsaAnton@users.noreply.github.com>
2024-08-29 09:48:28 +0300

    feat

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0){ 
+        n = 0 - n; 
+    }
+    let str = String(n); 
+    let arr = str.split(""); 
+    let reverseArr = []; 
+    let num = arr.length - 1; 
+    for(let i = 0; i < arr.length; i++){ 
+        reverseArr[i] = arr[num--]; 
+    }
+    let reverseStr = reverseArr.join(""); 
+    let reverseNumber = Number(reverseStr); 
+    return reverseNumber; 
 }
